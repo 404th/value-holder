a78eda15e29c9caee01a5016a9c7d8eece21e1d4
404th <umarov.doniyor.2002@gmail.com>
2024-12-18 18:00:45 +0500

    some changes

--- internal/config/config.go
+++ internal/config/config.go
@@ -44,6 +44,7 @@ func NewConfig() (cfg *Config, err error) {
 	cfg.ValueHolderProjectHost = cast.ToString(getDotEnvValueIfExistOrReturnDefault("ValueHolderProjectHost", "localhost"))
 	cfg.ValueHolderProjectPort = cast.ToUint32(getDotEnvValueIfExistOrReturnDefault("ValueHolderProjectPort", 8050))
 	cfg.ValueHolderProjectMode = cast.ToString(getDotEnvValueIfExistOrReturnDefault("ValueHolderProjectMode", "development"))
+
 	cfg.PostgresHost = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresHost", "0.0.0.0"))
 	cfg.PostgresPort = cast.ToUint32(getDotEnvValueIfExistOrReturnDefault("PostgresPort", 5432))
 	cfg.PostgresDatabase = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresDatabase", "postgres"))
@@ -51,9 +52,10 @@ func NewConfig() (cfg *Config, err error) {
 	cfg.PostgresPassword = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresPassword", "postgres"))
 	cfg.PostgresMaxConnections = cast.ToUint32(getDotEnvValueIfExistOrReturnDefault("PostgresMaxConnections", 100))
 	cfg.PostgresSSLMode = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresSSLMode", "disable"))
-	cfg.PostgresDockerContainerName = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresDockerContainerName", "postgres_docker"))
+	cfg.PostgresDockerContainerName = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresDockerContainerName", "value_holder_postgresql"))
 	cfg.PostgresDockerContainerUser = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresDockerContainerUser", "postgres"))
 	cfg.PostgresDockerContainerPassword = cast.ToString(getDotEnvValueIfExistOrReturnDefault("PostgresDockerContainerPassword", "super_secret_password"))
+
 	cfg.RedisHost = cast.ToString(getDotEnvValueIfExistOrReturnDefault("RedisHost", "localhost"))
 	cfg.RedisPort = cast.ToUint8(getDotEnvValueIfExistOrReturnDefault("RedisPort", 6379))
 	cfg.RedisDatabase = cast.ToString(getDotEnvValueIfExistOrReturnDefault("RedisDatabase", "redis_database"))
